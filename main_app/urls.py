4723d24fe80c3b5f6e0bdf93464537c4b78207c4
Andrew <andrewchau1993@gmail.com>
2024-09-13 10:24:51 -0700

    toys associated

--- main_app/urls.py
+++ main_app/urls.py
@@ -15,7 +15,11 @@ urlpatterns = [
         name='add-feeding'
     ),
   path('toys/create/', views.ToyCreate.as_view(), name='toy_create'),
-
+  path('toys/<int:pk>/', views.ToyDetail.as_view(), name='toy_detail'),
+  path('toys/', views.ToyList.as_view(), name='toy_index'),
+  path('toys/<int:pk>/update/', views.ToyUpdate.as_view(), name='toy_update'),
+  path('toys/<int:pk>/delete/', views.ToyDelete.as_view(), name='toy_delete'),
+  path('cats/<int:cat_id>/assoc_toy/<int:toy_id>/', views.assoc_toy, name='assoc_toy'),
 ]
 
 # cats/:catId
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect # type: ignore
 from django.views.generic.edit import CreateView, UpdateView, DeleteView # type: ignore
+from django.views.generic import ListView, DetailView # type: ignore
 from .models import Cat, Toy # type: ignore
 from .forms import FeedingForm
 
@@ -14,10 +15,12 @@ def cats_index(req):
     return render(req, 'cats/index.html', {'cats': cats})
 def cats_detail(req, cat_id):
     cat = Cat.objects.get(id=cat_id)
+    toys = Toy.objects.all()
     feeding_form = FeedingForm()
     return render(req, 'cats/detail.html', {
         'cat': cat, 
-        'feeding_form': feeding_form
+        'feeding_form': feeding_form,
+        'toys': toys
         })
 def add_feeding(req, cat_id):
     form = FeedingForm(req.POST)
@@ -27,9 +30,14 @@ def add_feeding(req, cat_id):
         new_feeding.save()
     return redirect('cat_detail', cat_id=cat_id)
 
+def assoc_toy(req, cat_id, toy_id):
+    Cat.objects.get(id=cat_id).toys.add(toy_id)
+    return redirect('cat_detail', cat_id=cat_id)
+
 class CatCreate(CreateView):
     model = Cat
-    fields = '__all__'
+    fields = ['name', 'breed', 'description', 'age']
+    
 class CatUpdate(UpdateView):
     model = Cat
     # Let's disallow the renaming of a cat by excluding the name field!
@@ -41,3 +49,17 @@ class CatDelete(DeleteView):
 class ToyCreate(CreateView):
     model = Toy
     fields = '__all__'
+    success_url = '/toys/'
+
+class ToyList(ListView):
+    model = Toy
+
+class ToyDetail(DetailView):
+    model = Toy
+    
+class ToyUpdate(UpdateView):
+    model = Toy
+    fields = '__all__'
+class ToyDelete(DeleteView):
+    model = Toy
+    success_url = '/toys/'
